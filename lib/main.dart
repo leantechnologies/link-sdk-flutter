87df0911bd353f0570085e7614f86a1bd877ca48
vladimirnechai <volodymyr.nechai@leantech.me>
2022-08-15 16:26:35 +0100

    init project

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:lean_example/widgets/link_sdk.dart';
+
+void main() {
+  runApp(const Demo());
+}
+
+class Demo extends StatelessWidget {
+  const Demo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var leanView = LeanViewWidget(
+      appToken: "d52f0c33-d721-480f-aa59-a37546baaae7",
+      callback: (message) => print(message),
+    );
+
+    Size size = MediaQuery.of(context).size;
+
+    return SafeArea(
+        child: Scaffold(
+            backgroundColor: Color(0xff00BCD1),
+            body: Column(
+              children: <Widget>[
+                TextButton(
+                  style: ButtonStyle(
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.blue),
+                  ),
+                  onPressed: () => leanView.connect(
+                      customerId: "8c73ae1a-ed23-4e16-84c6-eb0ffc476946",
+                      permissions: [
+                        "identity",
+                        "transactions",
+                        "balance",
+                        "accounts"
+                      ]),
+                  child: Text('Connect'),
+                ),
+                SizedBox(
+                    height: size.height - 68,
+                    width: size.height,
+                    child: leanView)
+              ],
+            )));
+  }
+}
