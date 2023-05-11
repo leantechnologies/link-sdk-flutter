b5e1eccb49fa57459d422b233781accb81c82a17
Kingdom Orjiewuru <kingdom.orjiewuru@gmail.com>
2023-05-11 13:06:16 +0400

    feat: setup enums

---
+++ lib/enums.dart
@@ -0,0 +1,60 @@
+// ignore_for_file: constant_identifier_names
+
+enum Config {
+  env,
+  sandbox,
+  version,
+  country,
+  language,
+  app_token,
+  show_logs,
+  implementation,
+}
+
+enum Params {
+  access_to,
+  account_id,
+  customer_id,
+  permissions,
+  access_from,
+  reconnect_id,
+  show_balances,
+  customization,
+  bank_identifier,
+  fail_redirect_url,
+  payment_intent_id,
+  payment_source_id,
+  initialization_url,
+  success_redirect_url,
+  payment_destination_id,
+}
+
+enum Methods {
+  pay,
+  link,
+  connect,
+  reconnect,
+  createBeneficiary,
+  createPaymentSource,
+  updatePaymentSource,
+}
+
+enum UserPermissions {
+  balance,
+  identity,
+  accounts,
+  payments,
+  transactions,
+
+  // KSA permissions
+  identities,
+  beneficiaries,
+  direct_debits,
+  standing_orders,
+  scheduled_payments,
+}
+
+enum Country {
+  sa,
+  ae,
+}
